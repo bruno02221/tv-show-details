c3b7450e1ee03a18c4caa86181a2cf9a352b1e7c
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-24 13:52:32 -0200

    Add info box component

--- src/TvShowDetailsScreen/InfoBox.js
+++ src/TvShowDetailsScreen/InfoBox.js
@@ -1,8 +1,38 @@
 import React from "react";
+import classnames from "classnames";
+import styles from "./InfoBox.css";
+import Tabs from "../Tabs";
 
-const InfoBox = () => {
+const InfoBox = ({ cast, synopsis, className, ...rest }) => {
+  const titles = ["Synopsis", "Cast"];
+  const contents = [
+    <Synopsis>{synopsis}</Synopsis>,
+    <Cast cast={cast} />
+  ]
+
   return (
-    <div>InfoBox</div>
+    <div {...rest} className={classnames(styles.infoBox, className)}>
+      <Tabs
+        titles={titles}
+        contents={contents}
+      />
+    </div>
+  );
+}
+
+const Synopsis = ({ children }) => {
+  return (
+    <p>{children}</p>
+  );
+}
+
+const Cast = ({ cast }) => {
+  return (
+    <div className={styles.cast}>
+      {cast.map(it => {
+        return <div key={it.id} className={styles.castPerson}>{it.name}</div>
+      })}
+    </div>
   );
 }
 
